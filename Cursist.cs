711ff05af590d06317ec452f9e09daa4c068fc1c
Z3bzonic <107925003+Z3bzonic@users.noreply.github.com>
2023-01-10 10:49:10 +0100

    Add files via upload

---
+++ Cursist.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace cursistToevoegen
+{
+    public class Cursist
+    {
+        public Cursist(string voornaam, string achternaam)
+        {
+            Voornaam = voornaam;
+            Achternaam = achternaam;
+        }
+
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+
+        public string Fullname { get => $"{Voornaam} {Achternaam}"; }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace cursistToevoegen.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Cursist> Studenten { get; set; } = new ObservableCollection<Cursist>()
+        {
+            new Cursist("sdfg", "gfds"),
+            new Cursist("xcvb", "bvc"),
+            new Cursist("xcvb", "sdfb")
+        };
+
+        private string _test = "Hello World";
+
+        public string Test
+
+        {
+            get { return _test; }
+            set
+            {
+                _test = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs(propertyName)); }
+        }
+
+        public void VoegStudentToe(string voornaam, string achternaam)
+        {
+            Studenten.Add(new Cursist(voornaam, achternaam));
+            Test = "Goodbye World";
+        }
+
+        //InputNaam.Clear();
+        //InputVoorNaam.Clear();
+        //StudentLijst.ItemsSource = null;
+        //StudentLijst.ItemsSource = studenten;
+    }
+}
